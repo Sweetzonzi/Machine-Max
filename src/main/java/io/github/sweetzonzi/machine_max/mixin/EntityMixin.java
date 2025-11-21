0e55d8246b6e4cc60dd080ece0631b06bb21440d
甜粽子 <1368003448@qq.com>
2025-11-21 17:47:27 +0800

    改进碰撞和下车表现

--- src/main/java/io/github/sweetzonzi/machine_max/mixin/EntityMixin.java
+++ src/main/java/io/github/sweetzonzi/machine_max/mixin/EntityMixin.java
@@ -33,7 +33,8 @@ import java.util.List;
 
 @Mixin(Entity.class)
 abstract public class EntityMixin extends AttachmentHolder implements IEntityMixin {
-    @Shadow public abstract Level level();
+    @Shadow
+    public abstract Level level();
 
     @Unique
     private AbstractControllableSubsystem machine_Max$controllingSubSystem;
@@ -43,7 +44,8 @@ abstract public class EntityMixin extends AttachmentHolder implements IEntityMix
     private Transform machine_Max$sweepTestStart = new Transform();
     @Unique
     private Transform machine_Max$sweepTestEnd = new Transform();
-
+    @Unique
+    private final Vec3 AXIS_YP = new Vec3(0, 1, 0);
     /**
      * <p>额外处理原版碰撞检测逻辑，使实体能够与物理体发生碰撞</p>
      * <p>Handle the original collision detection logic, allowing entities to collide with physical bodies.</p>
@@ -52,19 +54,20 @@ abstract public class EntityMixin extends AttachmentHolder implements IEntityMix
     private void onCollide(Vec3 vec, CallbackInfoReturnable<Vec3> cir) {
         // 获取原版碰撞结果
         Vec3 originalVec = cir.getReturnValue();
+        if (originalVec.lengthSqr() < 1e-4f) return; // 无运动时直接返回
         Entity entity = (Entity) (Object) this;
         AABB aabb = entity.getBoundingBox();
         // 跳过非必要的检测对象
         if (entity instanceof BlockAttachedEntity
                 || entity instanceof MMPartEntity
                 || (aabb.maxX - aabb.minX) * (aabb.maxY - aabb.minY) * (aabb.maxZ - aabb.minZ) < 0.001
-                || (entity instanceof Player player && !player.isAffectedByFluids())
+                || (entity instanceof Player player && !player.isAffectedByFluids()) // 飞行模式无碰撞
                 || ((IEntityMixin) entity).machine_Max$getControllingSubsystem() != null) return;
         // 调用物理引擎进行碰撞检测
         if (machine_Max$collideTestShape == null) {
             double x = (aabb.maxX - aabb.minX) / 2;
             double z = (aabb.maxZ - aabb.minZ) / 2;
-            float radius = (float) Math.sqrt(x * x + z * z) * 0.7f;
+            float radius = (float) Math.sqrt(x * x + z * z);
             float height = (float) (aabb.maxY - aabb.minY - radius * 2);
             machine_Max$collideTestShape = new CapsuleCollisionShape(radius, height > 0 ? height : 0.01f);
         }
@@ -72,7 +75,7 @@ abstract public class EntityMixin extends AttachmentHolder implements IEntityMix
         Vec3 delta = new Vec3(originalVec.x, originalVec.y, originalVec.z);
         Vec3 center = aabb.getCenter();
         if (delta.length() < 0.5f) delta = delta.normalize().scale(0.5f);
-        if(delta.length() < 0.498f) {
+        if (delta.length() < 0.498f) {
             //TODO:有时扫掠测试会报错，检查原因 会是因为delta某些情况下等于0吗？
             return; // 无运动时直接返回，也许能修复此问题
         }
@@ -81,7 +84,7 @@ abstract public class EntityMixin extends AttachmentHolder implements IEntityMix
         entity.level().getPhysicsLevel().getWorld().sweepTest(
                 machine_Max$collideTestShape,
                 machine_Max$sweepTestStart,
-                machine_Max$sweepTestEnd, results, 0.1f);
+                machine_Max$sweepTestEnd, results, 0.05f);
         if (results.isEmpty()) return;// 无碰撞结果时直接返回
         Vec3 normal = new Vec3(0, 1, 0);
         Vec3 movement = new Vec3(0, 0, 0);
@@ -107,22 +110,17 @@ abstract public class EntityMixin extends AttachmentHolder implements IEntityMix
         Vec3 normalComponent = normal.scale(dotProduct);
         // 减去法线方向投影，得到垂直法线方向的向量
         finalVec = originalVec.subtract(normalComponent);
-        // 计算运动方向与水平方向的夹角
-        float angle = (float) Math.acos(Math.clamp(finalVec.normalize().dot(new Vec3(finalVec.x, 0, finalVec.z).normalize()), -1, 1));
+        // 计算与水平方向的夹角
+        double angle = Math.acos(Math.clamp(normal.normalize().dot(AXIS_YP), -1, 1));
 //        if (entity instanceof Player)
-//            MachineMax.LOGGER.debug("angle: {}, normal: {}, originalVec: {} ,finalVec: {}", angle, normal, originalVec, finalVec);
-        if (angle * 180 / (float) Math.PI < 45f) {
-            //TODO: 配置文件控制是否全量碰撞或水平方向无碰撞
-            boolean noHorizontalCollision = false;
-            if (noHorizontalCollision || normal.dot(new Vec3(0, 1, 0)) > 0.7071f) {
-                //爬坡角度小于45°时
-                if (originalVec.horizontalDistanceSqr() > 0.0001f)
-                    //水平方向有运动时，取原始向量的长度，方便爬坡
-                    finalVec = new Vec3(originalVec.x, finalVec.horizontalDistance() * (float) Math.tan(angle), originalVec.z);
-                else
-                    //水平方向无运动时，保持静止
-                    finalVec = new Vec3(0, 0, 0);
-            }
+//            MachineMax.LOGGER.debug("angle: {}, normal: {}, originalVec: {} ,finalVec: {}", angle * 180 / Math.PI, normal, originalVec, finalVec);
+        if (angle * 180 / Math.PI < 45.0) { // 爬坡角度小于45°时
+            if (originalVec.horizontalDistanceSqr() > 0.0001f)
+                //水平方向有运动时，取原始向量的长度，方便爬坡
+                finalVec = finalVec.normalize().scale(originalVec.length());
+            else
+                //水平方向无运动时，保持静止
+                finalVec = new Vec3(0, 0, 0);
         }
         // 叠加刚体的运动
         movement = movement.scale(0.05);//速度转为单tick移动量
