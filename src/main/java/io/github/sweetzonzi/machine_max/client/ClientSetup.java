6d4009e7c2691701cc67a644e49a65f83f6fc76e
ArcherLee_rc <liruochen01@outlook.com>
2026-02-17 17:58:22 +0800

    使用第三方C输入库 jnativehook 进行原生级别输入监听 dll库测试环境导入，但还需要实现为用户自动安装

--- src/main/java/io/github/sweetzonzi/machine_max/client/ClientSetup.java
+++ src/main/java/io/github/sweetzonzi/machine_max/client/ClientSetup.java
@@ -2,6 +2,7 @@ package io.github.sweetzonzi.machine_max.client;
 
 import io.github.sweetzonzi.machine_max.MachineMax;
 import io.github.sweetzonzi.machine_max.client.input.CameraController;
+import io.github.sweetzonzi.machine_max.client.input.NativeKeyListener;
 import io.github.sweetzonzi.machine_max.common.registry.MMVisualEffects;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -14,6 +15,10 @@ public class ClientSetup {
     public static void init(FMLClientSetupEvent event) {
         CameraController.init();
         MMVisualEffects.init();
+        // 配置dll库在run路径下的相对子路径
+        System.setProperty("jnativehook.max_machine.path"
+                , "config/machine_max/libs/jNativeLib"); //todo: 应该实现自动从resource复制文件、生成相对子路径 | 参考早期外部表复制方法
+        NativeKeyListener.setUp();
         MachineMax.LOGGER.debug("Client setup complete");
     }
 }
---
+++ src/main/java/io/github/sweetzonzi/machine_max/client/input/NativeKeyListener.java
@@ -0,0 +1,76 @@
+package io.github.sweetzonzi.machine_max.client.input;
+
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
+import com.github.kwhat.jnativehook.mouse.NativeMouseInputListener;
+import com.github.kwhat.jnativehook.mouse.NativeMouseWheelEvent;
+import com.github.kwhat.jnativehook.mouse.NativeMouseWheelListener;
+
+public class NativeKeyListener implements NativeMouseInputListener, NativeMouseWheelListener, com.github.kwhat.jnativehook.keyboard.NativeKeyListener {
+	public void nativeMouseClicked(NativeMouseEvent e) {
+		System.out.println("Mouse Clicked: " + e.getClickCount());
+	}
+
+	public void nativeMousePressed(NativeMouseEvent e) {
+		System.out.println("Mouse Pressed: " + e.getButton());
+	}
+
+	public void nativeMouseReleased(NativeMouseEvent e) {
+		System.out.println("Mouse Released: " + e.getButton());
+	}
+
+	public void nativeMouseMoved(NativeMouseEvent e) {
+		System.out.println("Mouse Moved: " + e.getX() + ", " + e.getY());
+	}
+
+    public void nativeMouseWheelMoved(NativeMouseWheelEvent e) {
+        System.out.println("Mosue Wheel Moved: " + e.getWheelRotation());
+    }
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException nativeHookException) {
+                nativeHookException.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+    //todo 若运行报错，请查看ClientSetup中的备注
+    public static void setUp() {
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+        }
+
+        // Construct the example object.
+        NativeKeyListener example = new NativeKeyListener();
+
+        // Add the appropriate listeners.
+        GlobalScreen.addNativeMouseListener(example);
+        GlobalScreen.addNativeMouseMotionListener(example);
+        GlobalScreen.addNativeMouseWheelListener(example);
+        GlobalScreen.addNativeKeyListener(example);
+    }
+
+	public static void main(String[] args) {
+        setUp(); //java本地测试
+    }
+}
