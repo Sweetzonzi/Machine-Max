638d523c4d41c827716ccdacf7565922385e9f3d
甜粽子 <1368003448@qq.com>
2026-05-04 09:37:55 +0800

    - 添加wiki文档

--- docs/javascripts/mermaid-init.js
+++
@@ -1,8 +0,0 @@
-// 监听 MkDocs Material 的页面切换事件，重新渲染 Mermaid
-document$.subscribe(function() {
-    mermaid.initialize({ startOnLoad: false });
-    var nodes = document.querySelectorAll(".mermaid");
-    if (nodes.length > 0) {
-        mermaid.run({ nodes: nodes });
-    }
-});
