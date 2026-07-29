33fbb61f86b02cf0162536e7c4149d0c5d22e142
ArcherLee_rc <liruochen01@outlook.com>
2026-07-29 12:52:59 +0800

    试图修复SubsystemInteractPayload的多人广播问题，带审计

--- src/main/java/io/github/sweetzonzi/machine_max/network/payload/SubsystemInteractPayload.java
+++ src/main/java/io/github/sweetzonzi/machine_max/network/payload/SubsystemInteractPayload.java
@@ -5,33 +5,48 @@ import io.github.sweetzonzi.machine_max.common.mech.DestroyableObject;
 import io.github.sweetzonzi.machine_max.common.mech.ObjectManager;
 import io.github.sweetzonzi.machine_max.common.mech.vehicle.SubPart;
 import io.github.sweetzonzi.machine_max.common.mech.vehicle.interact.InteractBox;
+import lombok.Data;
+import lombok.experimental.Accessors;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
 
-public record SubsystemInteractPayload(
-        int subPartId,
-        String interactBoxName
-) implements CustomPacketPayload {
+import java.util.UUID;
+
+@Accessors(chain=true)
+@Data
+public class SubsystemInteractPayload implements CustomPacketPayload {
+    UUID excutePlayerUUID = null;
+    final int subPartId;
+    final String interactBoxName;
+    public SubsystemInteractPayload(int subPartId, String interactBoxName) {
+        this.subPartId = subPartId;
+        this.interactBoxName = interactBoxName;
+    }
     public static final Type<SubsystemInteractPayload> TYPE = new Type<>(
             ResourceLocation.fromNamespaceAndPath(MachineMax.MOD_ID, "subsystem_interact_payload")
     );
     public static final StreamCodec<FriendlyByteBuf, SubsystemInteractPayload> STREAM_CODEC = new StreamCodec<>() {
         @Override
         public @NotNull SubsystemInteractPayload decode(FriendlyByteBuf buffer) {
-            return new SubsystemInteractPayload(buffer.readInt(), buffer.readUtf());
+            return new SubsystemInteractPayload(buffer.readInt(), buffer.readUtf())
+                    .setExcutePlayerUUID(buffer.readBoolean() ? null : buffer.readUUID());
         }
 
         @Override
         public void encode(FriendlyByteBuf buffer, @NotNull SubsystemInteractPayload value) {
-            buffer.writeInt(value.subPartId());
-            buffer.writeUtf(value.interactBoxName());
+            buffer.writeInt(value.subPartId);
+            buffer.writeUtf(value.interactBoxName);
+            boolean uuidIsNull = value.excutePlayerUUID == null;
+            buffer.writeBoolean(uuidIsNull);
+            if (!uuidIsNull) buffer.writeUUID(value.excutePlayerUUID);
         }
     };
 
@@ -49,19 +64,30 @@ public record SubsystemInteractPayload(
         handle(payload, context);
         //将玩家输入转发给其他玩家，以在其他玩家客户端模拟自己的操作
         Player player = context.player();
-        PacketDistributor.sendToPlayersInDimension((ServerLevel) player.level(), payload);
+        PacketDistributor.sendToPlayersInDimension((ServerLevel) player.level(),
+                payload.setExcutePlayerUUID(player.getUUID())); // 传输前将当前触发者UUID填入
     }
 
     public static void handle(SubsystemInteractPayload payload, IPayloadContext context) {
-        DestroyableObject object = ObjectManager.getDestroyableObject(context.player().level(), payload.subPartId());
+        Level level = context.player().level();
+        String envStr = level.isClientSide() ? "客户端" : "服务端";
+        Player executingPlayer =
+                payload.getExcutePlayerUUID() == null ?
+                context.player() : //为空，说明是服务端
+                level.getPlayerByUUID(payload.excutePlayerUUID);
+        if (executingPlayer == null) {
+            MachineMax.LOGGER.warn("UUID 为 {} 的玩家在该{}未被发现, handle发送终止", payload.excutePlayerUUID, envStr);
+            return;
+        }
+        DestroyableObject object = ObjectManager.getDestroyableObject(level, payload.getSubPartId());
         if (object instanceof SubPart subPart) {
-            InteractBox interactBox = subPart.interactBoxes.get(payload.interactBoxName());
+            InteractBox interactBox = subPart.interactBoxes.get(payload.getInteractBoxName());
             if (interactBox != null)
-                context.enqueueWork(() -> interactBox.interact(context.player()));
+                context.enqueueWork(() -> interactBox.interact(executingPlayer));
             else
-                MachineMax.LOGGER.error("SubPart(id={})中未找到交互框{}，无法互动。", payload.subPartId(), payload.interactBoxName());
+                MachineMax.LOGGER.error("SubPart(id={})中未找到交互框{}，无法互动。", payload.getSubPartId(), payload.getInteractBoxName());
         } else
             MachineMax.LOGGER.error("维度{}中不存在SubPart(id={})，无法互动。",
-                    context.player().level().dimension().location(), payload.subPartId());
+                    level.dimension().location(), payload.getSubPartId());
     }
 }
