01d57ed6ad0a35e900d101c9e382452147eb667c
ArcherLee_rc <liruochen01@outlook.com>
2026-08-09 22:10:37 +0800

    获取当前可用载具列表

--- src/main/kotlin/io/github/sweetzonzi/machine_max/common/command/VehicleCommand.kt
+++ src/main/kotlin/io/github/sweetzonzi/machine_max/common/command/VehicleCommand.kt
@@ -3,14 +3,27 @@ package io.github.sweetzonzi.machine_max.common.command
 import cn.solarmoon.spark_core.command.BaseCommand
 import com.mojang.brigadier.context.CommandContext
 import io.github.sweetzonzi.machine_max.common.mech.ObjectManager
+import io.github.sweetzonzi.machine_max.external.MMDynamicRes
 import net.minecraft.commands.CommandBuildContext
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 
 class VehicleCommand(): BaseCommand("vehicle", 4) {
     override fun putExecution(context: CommandBuildContext) {
         builder.then(Commands.literal("clear").executes { clear(it) })
+        builder.then(Commands.literal("list_templates").executes { listTemplates(it) })
+    }
+
+    private fun listTemplates(ctx: CommandContext<CommandSourceStack>): Int {
+        val ids = MMDynamicRes.TEMPLATES.keys.toMutableList()
+        ids.sortWith(ResourceLocation::compareTo)
+        ctx.source.sendSuccess({ Component.literal("已注册载具模板数: ${ids.size}") }, false)
+        for (id in ids) {
+            ctx.source.sendSuccess({ Component.literal("  $id") }, false)
+        }
+        return 1
     }
 
     private fun clear(context: CommandContext<CommandSourceStack>): Int {
